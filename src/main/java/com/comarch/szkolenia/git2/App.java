aab45bc206dfd62ea5e3952ff30bcec638e94974
Mateusz <mat.bereda@gmail.com>
2025-05-11 15:13:06 +0200

    jakas zmiana 2

--- src/main/java/com/comarch/szkolenia/git2/App.java
+++ src/main/java/com/comarch/szkolenia/git2/App.java
@@ -3,5 +3,7 @@ package com.comarch.szkolenia.git2;
 public class App {
     public static void main(String[] args) {
         System.out.println("cos tam ");
+
+        System.out.println("cos nowego");
     }
 }
